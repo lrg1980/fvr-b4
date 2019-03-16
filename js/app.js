f4f10231773a0988066a267793d74ddad88d4315
lrg1980 <admin@binweb.net>
2019-03-16 13:58:51 -0300

    Finalizando FVR

---
+++ js/app.js
@@ -0,0 +1,11 @@
+window.onscroll = () => {
+     // console.log(window.scrollY);
+     let scroll = window.scrollY;
+
+     const headerScroll = document.querySelector('.header-scroll');
+     if(scroll > 300) {
+          headerScroll.classList.add('activo');
+     } else {
+          headerScroll.classList.remove('activo');
+     }
+}
